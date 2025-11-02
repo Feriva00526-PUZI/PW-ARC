ac254d3d027d5286e0734d68b1c371fbdd2508f1
CarlosCVG <L23550322@chihuahua2.tecnm.mx>
2025-11-02 12:10:42 -0600

    Vista de movimiento para los lugares termiada

---
+++ src/scripts/a_info_agency.js
@@ -0,0 +1,75 @@
+
+window.addEventListener("load", function () {
+
+    fetch("./../../components/header.html")
+        .then(response => response.text())
+        .then(data => {
+            document.body.insertAdjacentHTML("afterbegin", data);
+
+            const script = document.createElement("script");
+            script.src = "./../../scripts/header_script.js";
+            document.body.appendChild(script);
+
+            /*HEADER DINAMICO */
+            /*Cambio de la imagen del header */
+            const s_header = document.getElementById("s_header");
+            s_header.style.backgroundImage = "url(./../../media/images/layout/imgLayout15.jpg)";
+            s_header.style.backgroundPosition = "50% 70%";
+
+            /*Cambiar el titulo del header */
+            document.getElementById("n_h2").innerText = "Estadisticas de los Viajes";
+            document.getElementById("s_icon").setAttribute("src", "./../../media/images/icons/icon_arc.png");
+            const bnav = document.getElementById("underline_nav");
+
+            /*Agregar los elementos al nav */
+            /*Primero*/
+            const a1 = document.createElement("a");
+            a1.id = "a1";
+            a1.href = "./a_gestion_view.html";
+            const ai1 = document.createElement("img");
+            ai1.src = "./../../media/images/icons/iconAnav1.png";
+            ai1.classList.add("icon_nav");
+            a1.appendChild(ai1);
+            a1.append("Gestion de lugares");
+            bnav.appendChild(a1);
+            /*Segundo*/
+            const a2 = document.createElement("a");
+            a2.id = "a2";
+            a2.href = "./a_info_agency.html";
+            const ai2 = document.createElement("img");
+            ai2.src = "./../../media/images/icons/iconAnav2.png";
+            ai2.classList.add("icon_nav");
+            a2.appendChild(ai2);
+            a2.append("Estadisticas de los Viajes");
+            bnav.appendChild(a2);
+            /*Tercero*/
+            const a3 = document.createElement("a");
+            a3.id = "a3";
+            a3.href = "./a_info_events.html";
+            const ai3 = document.createElement("img");
+            ai3.src = "./../../media/images/icons/iconAnav3.png";
+            ai3.classList.add("icon_nav");
+            a3.appendChild(ai3);
+            a3.append("Estadisticas de los Eventos");
+            bnav.appendChild(a3);
+        });
+
+    /*Copiar y pegar eso para añadir el footer en la pagina que sea */
+    fetch("./../../components/footer.html")
+        .then(response => response.text())
+        .then(data => {
+            document.body.insertAdjacentHTML("beforeend", data);
+
+            /*Cambio del icono ARC (Solo para actualizar la ruta relativa) */
+            document.getElementById("f_icon").setAttribute("src", "./../../media/images/icons/icon_arc.png");
+
+            /*Cambio link del boton hacia la pagina About Us (Solo para actualizar la ruta relativa) */
+            document.querySelector(".f_link").href = "#";
+
+            /*Cambio de la imagen del footer derecho*/
+            const f_general = document.getElementById("f_general");
+            f_general.style.backgroundImage = "url(./../../media/images/layout/imgLayout16.jpg)";
+            /*Cambiar que parte de la imagen se ve, el primer 50 es horizontalmente(no cambiarlo) y el segundo es para la altura que se visualiza */
+            f_general.style.backgroundPosition = "50% 80%";
+        });
+});
--- src/scripts/ingreso_script.js
+++ src/scripts/ingreso_script.js
@@ -1,7 +1,9 @@
 window.addEventListener("load", function () {
     document.querySelector("body").style.backgroundImage = "none";
+    const datos_incorrectos = document.getElementById("datos_incorrectos");
     const boton_registro = document.getElementById("cambio_registro");
     const boton_login = document.getElementById("cambio_login");
+    const boton_acceder = document.getElementById("boton_acceder");
     const container = document.getElementById("contenedor");
 
     boton_registro.addEventListener("click", function () {
@@ -9,5 +11,24 @@ window.addEventListener("load", function () {
     });
     boton_login.addEventListener("click", function () {
         container.classList.remove("panel_derecho_activo");
+    });
+    boton_acceder.addEventListener("click", function (e) {
+        e.preventDefault();
+        const i_usuario = document.getElementById("i_usuario");
+        const i_password = document.getElementById("i_password");
+
+        if (i_usuario.value == "admin" && i_password.value == "admin") {
+            sessionStorage.setItem("admin_logeado", i_usuario.value);
+            window.location.href = "./admin/a_gestion_view.html";
+            //window.location.replace("./admin/a_gestion_view.html");
+        } else {
+            i_usuario.value = "";
+            i_password.value = "";
+            datos_incorrectos.showModal();
+        }
+    });
+    const close_datos_incorrectos = document.getElementById("close_datos_incorrectos");
+    close_datos_incorrectos.addEventListener("click", function () {
+        datos_incorrectos.close();
     });
 });
