f307c5850c7027c337c47c3761bf5ee4e5078a66
KMmiau <L23550381@chihuahua2.tecnm.mx>
2025-11-16 13:20:35 -0600

    foreach

--- src/scripts/UsuarioViajes_Lugares.js
+++ src/scripts/UsuarioViajes_Lugares.js
@@ -3,13 +3,18 @@ window.addEventListener("load", function(){
     let contenedorLugar = document.getElementById("selecciones");
     let ol = document.createElement("ol");
     fetch("./../../data/logic/lugarLogic.php").then(response => response.json()).then(data => {
-    for(let i = 0; i <= data.zona.length; i++){
+        const lugares = data.lugares;
+
+    lugares.forEach(data => {
         let li = document.createElement("li");
+        console.log(data[1].zona);
         let liTxt = document.createTextNode(data[i].zona);
         li.appendChild(liTxt);
         li.setAttribute("class", "cards");
         ol.appendChild(li);
-    }
+    });
+        
+    
     contenedorLugar.appendChild(ol);
     }).catch(error => {
         alert("Error de conexión al servidor. No se pudieron obtener los lugares.");
