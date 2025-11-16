f823f0f70895f866c38afbfce68e2eec0f5fccbd
KMmiau <L23550381@chihuahua2.tecnm.mx>
2025-11-16 12:33:43 -0600

    correccion

--- src/scripts/UsuarioViajes_Lugares.js
+++ src/scripts/UsuarioViajes_Lugares.js
@@ -3,11 +3,13 @@ import {
 } from "../scripts/dao/daoEventos.js";
 window.addEventListener("load", function(){
     let contenedorLugar = document.getElementById("selecciones");
+    let ol = document.createElement("ol");
     for(let i = 0; i <= obtenerTodosLugares.length; i++){
         let li = document.createElement("li");
         let liTxt = document.createTextNode(obtenerTodosLugares[i]);
         li.appendChild(liTxt);
         li.setAttribute("class", "cards");
-        contenedorLugar.appendChild(li);
+        ol.appendChild(li);
     }
+    contenedorLugar.appendChild(ol);
 })
