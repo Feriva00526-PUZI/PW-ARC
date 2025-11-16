d5fe6a665cd45eb7215c7c48b7ec793434ce072b
KMmiau <L23550381@chihuahua2.tecnm.mx>
2025-11-16 13:22:43 -0600

    correcto

--- src/scripts/UsuarioViajes_Lugares.js
+++ src/scripts/UsuarioViajes_Lugares.js
@@ -3,6 +3,8 @@ window.addEventListener("load", function(){
     let contenedorLugar = document.getElementById("selecciones");
     let ol = document.createElement("ol");
     fetch("./../../data/logic/lugarLogic.php").then(response => response.json()).then(data => {
+if (data.correcto && data.lugares) {
+
         const lugares = data.lugares;
 
     lugares.forEach(data => {
@@ -16,6 +18,9 @@ window.addEventListener("load", function(){
         
     
     contenedorLugar.appendChild(ol);
+    } else {
+            console.log("Hubo error en el if de correcto y lugares en a_gestion_viewJS");
+        }
     }).catch(error => {
         alert("Error de conexión al servidor. No se pudieron obtener los lugares.");
     });
