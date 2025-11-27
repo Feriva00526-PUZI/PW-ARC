89231585f51f1d8cd712fab5bbf37298025caa50
CarlosCVG <L23550322@chihuahua2.tecnm.mx>
2025-11-26 19:20:39 -0600

    cambios clase

--- src/data/DAO/actividadDAO.php
+++ src/data/DAO/actividadDAO.php
@@ -1,25 +1,28 @@
 <?php
 require_once "./../conexion.php";
 
-class actividadDAO{
+class actividadDAO
+{
 
     private $id;
     private $conexion;
 
-    public function __construct(){
+    public function __construct()
+    {
         $conn = new conexion();
         $this->conexion = $conn->getConexion();
     }
 
-    public function getActividades(){
-        try{
+    public function getActividades()
+    {
+        try {
             $sql = "SELECT * FROM tipoactividad";
             $stmt = $this->conexion->prepare($sql);
             $stmt->execute();
 
             $tipoActividad = $stmt->fetchALL(PDO::FETCH_ASSOC);
             return $tipoActividad;
-        } catch(PDOException $e){
+        } catch (PDOException $e) {
             throw new Exception("Error al obtener tipo de actividades: " . $e->getMessage());
         }
     }
