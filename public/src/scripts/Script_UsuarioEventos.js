f81db7bb6573b3581bcc50c6f70c8db9eab2330e
CarlosCVG <L23550322@chihuahua2.tecnm.mx>
2025-11-30 17:50:14 -0600

    no cambie nada

--- public/src/scripts/Script_UsuarioEventos.js
+++ public/src/scripts/Script_UsuarioEventos.js
@@ -1,9 +1,9 @@
 window.addEventListener("load", function () {
     const usuarioSession = sessionStorage.getItem("usuario_logeado");
-  if (usuarioSession == null) {
-    window.location.href = "./../../../index.html";
-    return;
-  }
+    if (usuarioSession == null) {
+        window.location.href = "./../../../index.html";
+        return;
+    }
     let contenedorLugar = document.getElementById("selecciones");
     let contenedorPaquete = document.getElementById("paquete");
     let paqcont = 1;
@@ -35,52 +35,53 @@ window.addEventListener("load", function () {
                     //let id_evento = data.id_evento;
                     //console.log(id_lugar);
                     //fetch(`./../../data/logic/PaquetesLogic.php?id_lugar=${id_lugar}`).then(response => response.json()).then(data => {
-                        //if (data.correcto && data.paquete) {
-                        //const paquetes = data.paquetes;
-                        contenedorPaquete.innerHTML = "";
-                        //paquetes.forEach(data => {
-                            let div = document.createElement("div");
-                            //let divTxt = document.createTextNode("Reservar,"  + " Precio: " + data.precio_boleto);
-                            //div.appendChild(divTxt);
-                            //div.setAttribute("id", "paq" + paqcont);
-                            paqcont++;
-                            //div.setAttribute("class", "minicard");
-                            div.innerHTML = `<div class="minicard"> <p style="font-size: 20px">Reservar</p>
+                    //if (data.correcto && data.paquete) {
+                    //const paquetes = data.paquetes;
+                    contenedorPaquete.innerHTML = "";
+                    //paquetes.forEach(data => {
+                    let div = document.createElement("div");
+                    //let divTxt = document.createTextNode("Reservar,"  + " Precio: " + data.precio_boleto);
+                    //div.appendChild(divTxt);
+                    //div.setAttribute("id", "paq" + paqcont);
+                    paqcont++;
+                    //div.setAttribute("class", "minicard");
+                    div.innerHTML = `<div class="minicard"> <p style="font-size: 20px">Reservar</p>
                             <br> <p style="font-style: italic">Precio: $${data.precio_boleto}</p> </div>`;
-                            contenedorPaquete.appendChild(div);
-                            const imgMapa = document.getElementById("imgMapa");
-                            imgMapa.src = `./../../media/images/events/${data.id_evento}.jpg`;
-                            let con = 0;
-                            div.addEventListener("click", function(){
-                                fecha.innerText = "Fecha del evento: " + data.fecha_evento;
-                                hora.innerText = "Hora del evento: " + data.hora_evento;
-                                modal.showModal();
-                                
-                                btn_aceptar.addEventListener("click", () => {
-                                    console.log(con);
-                                if(con == 0){
+                    contenedorPaquete.appendChild(div);
+                    const imgMapa = document.getElementById("imgMapa");
+                    imgMapa.src = `./../../media/images/events/${data.id_evento}.jpg`;
+                    let con = 0;
+                    div.addEventListener("click", function () {
+                        fecha.innerText = "Fecha del evento: " + data.fecha_evento;
+                        hora.innerText = "Hora del evento: " + data.hora_evento;
+                        modal.showModal();
+
+                        btn_aceptar.addEventListener("click", () => {
+                            console.log(con);
+                            if (con == 0) {
                                 const miArray = JSON.parse(usuarioSession);
                                 let id_evento = data.id_evento;
-                                let id_cliente = miArray.id_cliente; 
+                                let id_cliente = miArray.id_cliente;
                                 //let id_paquete = data.id_paquete; 
-                                let estado = "pendiente"; 
-                                
+                                let estado = "pendiente";
+
                                 const formData = new FormData();
-                                formData.append("id_evento",id_evento);
-                                formData.append("id_cliente",id_cliente);
+                                formData.append("id_evento", id_evento);
+                                formData.append("id_cliente", id_cliente);
                                 //formData.append("id_paquete",id_paquete);
-                                formData.append("estado",estado);
-                                
+                                formData.append("estado", estado);
+
                                 fetch(`./../../data/logic/CrearReservacionLogic.php`, {
-                                        method: 'POST',
-                                        body: formData
-                                    }).then(response => response.json()).then(data => {
-                                        console.log("funcionó??"+ data.mensaje);
-                                    });
-                                    con++;
-                                modal.close();}
-    //});
-                                
+                                    method: 'POST',
+                                    body: formData
+                                }).then(response => response.json()).then(data => {
+                                    console.log("funcionó??" + data.mensaje);
+                                });
+                                con++;
+                                modal.close();
+                            }
+                            //});
+
                             //});
                         }
 
@@ -107,7 +108,7 @@ window.addEventListener("load", function () {
     }).catch(error => {
         alert("Error de conexiÃ³n al servidor. No se pudieron obtener los lugares.");
     });
-btn_noAceptar.addEventListener("click", () => {
+    btn_noAceptar.addEventListener("click", () => {
         modal.close();
     });
 
